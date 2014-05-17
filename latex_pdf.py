2d4814b5b2ce41f93f534478e8d582623d1fb12e
Marty Miller <millerda@berea.edu>
2014-05-17 19:50:03 +0200

    Adding more comments

--- latex_pdf.py
+++ latex_pdf.py
@@ -6,6 +6,7 @@ from machine import *
 
 class pdfCreator():
 
+    # declare and define all variables in the constructor
     def __init__(self,dn,fn,inv):
         self.invention = inv
         self.file = self.create_TeX_file(dn,fn)
@@ -16,6 +17,7 @@ class pdfCreator():
         self.claims = self.create_claims()
         self.file_contents = self.create_LaTeX()
 
+    # used to open a new folder and open appropriate file
     def create_TeX_file(self,dname,fname):
 
         if not os.path.exists(dname):
@@ -23,62 +25,57 @@ class pdfCreator():
 
         return open(dname + "/" + fname + ".tex","a+")
 
+    # assemble the full LaTeX text
     def create_LaTeX(self):
-        # write article class
-        text = "\\documentclass[english]{uspatent}"
-        # begin document
-        text += "\n\\begin{document}"
-        # make title
+        text = "\\documentclass[english]{uspatent}\n\\begin{document}"
+        
         text += self.title
-            
-        # make abstract section
+        
         text += self.abstract
 
-        # make drawings section
         text += self.illustrations
 
-        # make embodiments section
         text += self.description
 
-        # make claims section
         text += self.claims
 
-        # end document
         text += "\n\\end{document}"
 
         return text
 
+    # assemble the title featuers
     def create_title(self):
-        # title
         title = "\n\\title{" + self.invention.title + "}"
-        # date
         title += "\n\\date{\\today}"
-        # inventor
         title += "\n\\inventor{First Named Inventor}"
-        # write the title
         title += "\n\\maketitle"
 
         return title
 
+    # put the abstract together
     def create_abstract(self):
         abs = "\n\\patentSection{Abstract}"
         abs += "\n\\patentParagraph " + self.invention.abstract
 
         return abs
 
+    # collect image descriptions
     def create_illustrations(self):
         ill = "\n\\patentSection{Brief Description of the Drawings}"
         for i in self.invention.illustrations:
+                    # seperate paragraph for each, maybe not necessary
             ill += "\n\\patentParagraph " + i
 
         return ill
 
+    # put description together
     def create_description(self):
         desc = "\n\\patentSection{Detailed Description of the Preferred Embodiments}"
         desc += "\n\\patentParagraph " + self.invention.description
 
         return desc
 
+    # assemble the claims together
     def create_claims(self):
         cla = "\n\\patentClaimsStart"
         
@@ -89,11 +86,12 @@ class pdfCreator():
 
         return cla
 
+    # write the entire text to the file
     def write_LaTeX_to_file(self):
+                                            # to fix paragraph formatting
         self.file.write(self.file_contents.replace("\n\n","\n\\patentParagraph "))
 
-    
-            
+    # function to compile the LaTeX formatting, not working yet
     #def compile_LaTeX(self):
         #process = subprocess.call("pdflatex test/test.tex", shell=True)
 
